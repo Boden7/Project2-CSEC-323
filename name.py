737824e5efbc96f060b1225fd4b8f89754ad56c2
PeacockHunter <peacockhmp@gmail.com>
2024-11-02 16:04:23 -0400

    Update name.py

--- name.py
+++ name.py
@@ -11,9 +11,6 @@ class Name:
       
       self._firstName = first_name 
       self._lastName = last_name 
-
-    def __str__(self):
-      return f"{self._firstName} {self._lastName}"
     
     # An accessor/getter method for the first name
     #
@@ -38,7 +35,7 @@ class Name:
       assert last.isalpha(), "The last name must not contain any special characters."
       assert len(last) > 0 and len(last) <= 40, "The last name must be a valid length."
       
-      self._lastName = last
+      self._lastName = last 
 
     # A mutator/setter method for the first name
     # @param first: The new first name for the Bank Account (String: default is an empty string)
@@ -50,3 +47,8 @@ class Name:
       assert len(first) > 0 and len(first) <= 25, "The first name must be a valid length."      
       
       self._firstName = first
+
+     # repr method for string representation of the clients name:
+     # Hunter 
+     def __repr__(self):
+      return f"{self._firstName} {self._lastName}"
